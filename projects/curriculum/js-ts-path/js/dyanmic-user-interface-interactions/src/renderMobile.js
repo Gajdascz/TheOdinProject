2db32be91504c973eb8acc9954ec32b52c36adde
gajdascz <dev.gajdascz@gmail.com>
2024-09-10 23:19:10 -0400

    Remove typo dir

--- projects/curriculum/js-ts-path/js/dyanmic-user-interface-interactions/src/renderMobile.js
+++
@@ -1,8 +0,0 @@
-const page = document.querySelector('.mobile-webpage');
-
-const renderMobilePage = (pageTitle, pageText) => {
-  page.querySelector('.mobile-page-title').textContent = pageTitle;
-  page.querySelector('.mobile-page-text').textContent = pageText;
-};
-
-export { renderMobilePage };
